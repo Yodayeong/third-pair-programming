5a1a29dc3057d372eb07f75b376adc6f9dc13b83
dortkthf <dortkthf@gmail.com>
2022-09-30 15:44:55 +0900

    django

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('new/',views.new, name='new'),
     path('create/', views.create, name='create'),
+    path('detail/<int:pk>', views.detail, name='detail'),
+    path('delete/<int:pk>',views.delete, name='delete'),
+    path('update/<int:pk>',views.update, name='update'),
+    path('updateF/<int:pk>',views.updateF, name='updateF')
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -11,11 +11,45 @@ def new(request):
     return render(request,'board/new.html')
 
 def create(request):
-    boards = request.GET.get('board')
+    new_board = Board()
+    new_board.title = request.GET.get('title')
+    new_board.user = request.GET.get('user')
+    new_board.content = request.GET.get('content')
+    new_board.boards = request.GET.get('board')
+    new_board.save()
+    
+    return redirect('board:detail',new_board.id)
+
+def detail(request, pk):
+    board = Board.objects.get(pk=pk)
+    context = {
+        'board' : board,
+    }
+    return render(request,'board/detail.html', context)
+
+def delete(request, pk):
+    board = Board.objects.get(pk=pk)
+    board.delete()
+    return redirect('board:index')
+
+def update(request, pk):
+    board = Board.objects.get(pk=pk)
+    context = {
+        'board':board,
+    }
+    return render(request,'board/update.html',context)
+
+def updateF(request,pk):
+    content = request.GET.get('content')
     title = request.GET.get('title')
     user = request.GET.get('user')
-    content = request.GET.get('content')
+    b = request.GET.get('board')
 
-    Board.objects.create(boards=boards, title=title, user=user, content=content)
+    board = Board.objects.get(pk=pk)
+    board.content = content
+    board.title = title
+    board.user = user
+    board.boards = b
+    board.save()
 
-    return redirect('board:index')
+    return redirect('board:detail',pk)
