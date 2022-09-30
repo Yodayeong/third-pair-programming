a69efb037d851ae8e45665a090b4fe923c54aeba
dortkthf <dortkthf@gmail.com>
2022-09-30 11:21:14 +0900

    영화리뷰 페이지 페어프로젝트

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Board
 # Create your views here.
+
+def index(request):
+    return render(request,'board/index.html')
+
+def new(request):
+    return render(request,'board/new.html')
